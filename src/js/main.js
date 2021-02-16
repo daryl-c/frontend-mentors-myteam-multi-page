336e5531f499b9e2c64e3eee2ecb977b848eccbb
Daryl Carr <darylcarr@live.co.uk>
2021-02-16 11:21:53 +0200

    Add probably naive trap focus to side bar

--- src/js/main.js
+++ src/js/main.js
@@ -2,29 +2,70 @@ const openBtn = document.getElementById('nav-open');
 const closeBtn = document.getElementById('nav-close');
 const nav = document.querySelector('.nav');
 const navOverlay = document.querySelector('.nav__overlay');
+const navList = nav.querySelector('.nav__inner');
 const siteHeader = document.querySelector('.site-head');
+
+const BREAKPOINT_TAB_PORT = 48 * 16;
+const BREAKPOINT_DESKTOP_SMALL = 90 * 16;
+const KEY_CODE_MAP = {
+    ESCAPE: 'Escape',
+    TAB: 'Tab'
+};
+
+const getAllFocusableElements = parent => {
+    if (!parent) {
+        console.warn('You need to pass a parent HTMLElement');
+        return []; // Return array so length queries will work
+    }
+
+    return parent.querySelectorAll(
+        'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'
+    );
+};
+
+function trapFocus(event) {
+    console.log(event.key, event.shiftKey);
+    if (event.key === KEY_CODE_MAP.ESCAPE) {
+        closeMenu();
+    }
+    const focusables = getAllFocusableElements(nav.querySelector('.nav__inner'));
+    const firstFocusable = focusables[0];
+    const lastFocusable = focusables[focusables.length - 1];
+
+    const isCurrentlyOnFirstElement = document.activeElement === firstFocusable;
+    const isCurrentlyOnLastElement = document.activeElement === lastFocusable
+
+    if (isCurrentlyOnFirstElement && event.key === KEY_CODE_MAP.TAB && event.shiftKey) {
+        event.preventDefault();
+    lastFocusable.focus();
+    }
+
+    if (isCurrentlyOnLastElement && event.key === KEY_CODE_MAP.TAB && !event.shiftKey) {
+        event.preventDefault();
+        firstFocusable.focus();
+    }
+}
 
 function closeMenu () {
     nav.classList.remove('nav--open');
     openBtn.setAttribute('aria-expanded', false);
+    window.removeEventListener('keydown', trapFocus);
 }
 
-openBtn.addEventListener('click', () => {
-    openBtn.setAttribute('aria-expanded', true);
-    nav.classList.add('nav--open');
-    navOverlay.addEventListener('click', closeMenu);
-});
-
-closeBtn.addEventListener('click', () => {
-    closeMenu();
-    navOverlay.removeEventListener('click', closeMenu);
-});
-
 const resizeObserver = new ResizeObserver(() => {
-    if (siteHeader.getBoundingClientRect().width) {
+    if (siteHeader.getBoundingClientRect().width > BREAKPOINT_TAB_PORT) {
         closeMenu();
-        navOverlay.removeEventListener('click', closeMenu);
     }
 });
 
 resizeObserver.observe(siteHeader);
+openBtn.addEventListener('click', () => {
+    openBtn.setAttribute('aria-expanded', true);
+    nav.classList.add('nav--open');
+    window.addEventListener('keydown', trapFocus);
+
+    const firstElement = getAllFocusableElements(nav.querySelector('.nav__inner'))[0];
+    firstElement.focus();
+});
+closeBtn.addEventListener('click', closeMenu);
+navOverlay.addEventListener('click', closeMenu);
